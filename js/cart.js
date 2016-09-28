cfc6789f762a0cf34c2e235605cb130d4c40f9d7
Lauren Templeton <etothel0514@gmail.com>
2016-09-28 19:22:44 -0400

    created cart.html page as placeholder and wired get/post info

---
+++ js/cart.js
@@ -0,0 +1,26 @@
+//get from query-string the value for select and set it to the input value
+
+var inputHTML = function (name, value){
+  return '<div><span>' + name + '</span><input type="text" name="' + name +'" value="' + value +'"></div>';
+}
+
+var queryString = window.location.search.substr(1).split('&');
+
+
+for (var i = 0; i < queryString.length; i++) {
+  if (queryString[i]){
+     var parts = queryString[i].split('=');
+
+     if (parts.length === 2){
+      var key = parts[0];
+      var value = parts[1];
+      //<input type="text" name="select" value="">
+
+      $('form').append($(inputHTML(key, value)));
+     }
+
+
+  }
+}
+
+$('form').append('<button>Submit</button>');
